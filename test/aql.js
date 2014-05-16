0cf52532235835ae3540f87050f68f08e7bec8a7
Thomas Lukacs <tlukacs@boerse-go.de>
2014-05-16 11:57:24 +0200

    double number bugfix

--- test/aql.js
+++ test/aql.js
@@ -253,16 +253,20 @@ describe('Aql', function() {
         
         alerts = [
             ['quote.133962[4.last:{<7200}]',
-             'quote#133962#4#last:{<7200}'],
+              'quote#133962#4#last:{<7200}'],
             ['status[id:1&type[id:2&name:abc]&power:{>9000}]',
-             'status#id:1&status#type#id:2&status#type#name:abc&status#power:{>9000}'],
+              'status#id:1&status#type#id:2&status#type#name:abc&status#power:{>9000}'],
             ['chartpattern.patternData[(instrumentId:122118|instrumentId:122117|instrumentId:133978)&(patternType:2|patternType:2001|patternType:2012)]',
-             '(chartpattern#patternData#instrumentId:122118' +
-             '|chartpattern#patternData#instrumentId:122117' +
-             '|chartpattern#patternData#instrumentId:133978)' +
-             '&(chartpattern#patternData#patternType:2' +
-             '|chartpattern#patternData#patternType:2001' +
-             '|chartpattern#patternData#patternType:2012)']
+              '(chartpattern#patternData#instrumentId:122118' +
+              '|chartpattern#patternData#instrumentId:122117' +
+              '|chartpattern#patternData#instrumentId:133978)' +
+              '&(chartpattern#patternData#patternType:2' +
+              '|chartpattern#patternData#patternType:2001' +
+              '|chartpattern#patternData#patternType:2012)'],
+            ['chartpattern.patternData[instrumentId:122118&patternType:2&distPercentQuotePotentialFromLevel1:{>3.5}]',
+              'chartpattern#patternData#instrumentId:122118' +
+              '&chartpattern#patternData#patternType:2' +
+              '&chartpattern#patternData#distPercentQuotePotentialFromLevel1:{>3.5}']
         ];
         
         function factory(alert) {
