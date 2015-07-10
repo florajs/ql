f94312eff74fa52174bb756e61bda753a13ad4d2
Nico Kaiser <nico.kaiser@boerse-go.de>
2015-07-10 10:53:05 +0200

    Fix minor linting issues, add .eslintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         },
 
         eslint: {
-            target: ['lib/**/*.js']
+            target: ['*.js', 'lib/**/*.js']
         },
 
         clean: {
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var assert              = require('./error/assert'),
     extend              = require('./config/extend'),
     tokenizer           = require('./tokenizer'),
